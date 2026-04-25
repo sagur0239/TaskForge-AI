2b308ff40444de17345c4cfcbe74f1817d506233
sagur0239 <mdsagurali.bd@gmail.com>
2026-04-25 12:04:55 +0600

    app.py

--- app.py
+++ app.py
@@ -40,4 +40,6 @@ def ask_ai():
         return render_template('index.html', error=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
